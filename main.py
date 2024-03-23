4fadf933e2b7a2c71335267ccd61d7f85479f5a4
Goh Yong Jie <gohyongjie1112@gmail.com>
2024-03-23 21:51:33 +0800

    feat: add show and load functions

--- main.py
+++ main.py
@@ -40,3 +40,23 @@ class URL:
         body = response.read()
         s.close()
         return body
+
+def show(body):
+    in_tag = False
+    # Example: <a href="http://www.example.com">Example</a>
+    #          ^ in_tag = True                 ^ in_tag = False, print "Example"
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+def load(url):
+    body = url.request()
+    show(body)
+
+if __name__ == "__main__":
+    import sys
+    load(URL(sys.argv[1]))
